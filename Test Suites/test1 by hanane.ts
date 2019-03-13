2f30e3fc7467025dd73fa5caa14997cee5b9975c
JabbarHanane <jabbarhanane11@gmail.com>
2019-03-13 22:57:58 +0000

    add test 1

---
+++ Test Suites/test1 by hanane.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test1 by hanane</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0e4efc7a-5132-46e7-892b-6b3f90c5d9f7</testSuiteGuid>
+</TestSuiteEntity>
